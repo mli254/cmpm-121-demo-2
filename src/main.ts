ebf444a7273450ea9a1f6daca150dd2b817f1cf0
mli254 <mli254@ucsc.edu>
2023-10-16 18:58:03 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Game Title";
-
+const gameName = "Sticky Sketch";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
