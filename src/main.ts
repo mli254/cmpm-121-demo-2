3be3b3becf7ff12c1a5247112c633b0263518d2a
mli254 <mli254@ucsc.edu>
2023-10-16 19:11:16 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,40 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+const ctx = canvas.getContext("2d")!;
+
+const cursor = { active: false, x: 0, y: 0 };
+
+//const paths = [];
+
+canvas.addEventListener("mousedown", (event) => {
+  cursor.active = true;
+  cursor.x = event.offsetX;
+  cursor.y = event.offsetY;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+
+const canvasStartX = 0;
+const canvasStartY = 0;
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(canvasStartX, canvasStartY, canvas.width, canvas.height);
+});
